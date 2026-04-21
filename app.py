3741380a9bcd8b6bed6bb1df5dcf5efaa2f1412f
dongmin <dongmin914@gmail.com>
2026-04-21 15:47:21 +0200

    Add MA200 line to chart for 이격도 visualization

--- app.py
+++ app.py
@@ -250,8 +250,13 @@ def _render_chart(selected_ticker: str, selected_name: str, tf: str):
             "studies": [
               "BB@tv-basicstudies",
               "IchimokuCloud@tv-basicstudies",
-              "RSI@tv-basicstudies"
+              "RSI@tv-basicstudies",
+              {{"id": "MASimple@tv-basicstudies", "inputs": {{"length": 200}}}}
             ],
+            "studies_overrides": {{
+              "Moving Average.plot.color": "#e91e63",
+              "Moving Average.plot.linewidth": 2
+            }},
             "container_id": "tv_chart"
           }});
         </script>
